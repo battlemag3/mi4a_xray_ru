53a30ab7ccf64c76fcb169ffdc1172f5978a2f8f
M-K-S-P <miha.kokh@gmail.com>
2026-04-03 00:22:12 +0700

    variables

--- install-passwall-panel.sh
+++ install-passwall-panel.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+REPO='battlemag3/mi4a_xray_ru'
+direct_ip_path='Re-filter-lists/ipsum.lst'
+direct_host_path='Re-filter-lists/domains_all.lst'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;37m'
+NC='\033[0m' # No Color
 service passwall stop
 
 cd
